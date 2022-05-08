3ebbd8a265794b3b9f96fcfb3c21d67cf730c19e
cicero884 <joy861225@gmail.com>
2022-05-08 14:21:52 +0800

    Hello

---
+++ hello.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <assert.h>
+ 
+int main()
+{
+	// Defining variables
+	int a;
+ 
+	// Assigning value of a=2
+	a = 2;
+ 
+	// Displaying the value of a
+	printf("Value of a is:%d\n", a);
+ 
+	// assert function will not exit till the value of a =0
+	assert(a);
+ 
+	// now change the value of a=0
+	a = 0;
+ 
+	// Displaying the value of a
+	printf("Value of a is:%d\n\n", a);
+ 
+	// again calling the function with different parameter
+	assert(a);
+ 
+	return 0;
+ 
+}
